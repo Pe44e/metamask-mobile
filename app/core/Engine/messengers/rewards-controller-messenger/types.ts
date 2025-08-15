d6a13595503842e6e8882644cb10b9c5dfd57d6c
sophieqgu <sophieqgu@gmail.com>
2025-08-14 20:30:58 -0400

    Update state after successful opt-in

--- app/core/Engine/messengers/rewards-controller-messenger/types.ts
+++ app/core/Engine/messengers/rewards-controller-messenger/types.ts
@@ -24,8 +24,17 @@ export type RewardsControllerMessengerActions =
   | ControllerGetStateAction<'RewardsController', RewardsControllerState>;
 
 /**
+ * RewardsController specific events
+ */
+export interface RewardsControllerAccountOptInEvent {
+  type: 'RewardsController:accountOptIn';
+  payload: [string]; // account address
+}
+
+/**
  * Events that the RewardsController messenger can emit
  */
 export type RewardsControllerMessengerEvents =
   | AccountsControllerSelectedAccountChangeEvent
-  | KeyringControllerUnlockEvent;
+  | KeyringControllerUnlockEvent
+  | RewardsControllerAccountOptInEvent;
